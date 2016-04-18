770b2d73a1662f8b1a5efb60293ab7e422acf934
Takanori TANIGUCHI <askme@pretendco.net>
2016-04-18 23:23:34 +0900

    add python dir

---
+++ python/first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+print sys.argv[0]
+
+print sys.argv[1]
+print sys.argv[2]
+
+
+a = [66.25, 333, 333, 1, 1234.5]
+print (a.count(333), a.count(66.25))
+print (r"this is \n comment.")
+print ("this is \n comment. ")
+
+
+dir()
