de6a9773bcb0ced22ea7678fb2ca1b09ea642c8c
TANIGUCHI Takanori <askme@pretendco.net>
2021-10-28 19:39:33 +0900

    fmted

--- go/date-time.go
+++ go/date-time.go
@@ -6,22 +6,22 @@ import (
 )
 
 func main() {
-// 	const layout1 = "Now, Monday Jan 02 15:04:05 JST 2006"
-// 	const layout2 = "2006-01-02 15:04:05 JST"
-// 	const layout3 = "2006-01-02 15:04:05"
-// 	nowtime := time.Now()
-// 	fmt.Println(nowtime)
-// 	fmt.Println(nowtime.Format(layout1))
-// 	fmt.Println(nowtime.Format(layout2))
-// 	fmt.Println(nowtime.Format(layout3))
-// 	fmt.Println(nowtime.Format(time.RFC3339))
+	// 	const layout1 = "Now, Monday Jan 02 15:04:05 JST 2006"
+	// 	const layout2 = "2006-01-02 15:04:05 JST"
+	// 	const layout3 = "2006-01-02 15:04:05"
+	// 	nowtime := time.Now()
+	// 	fmt.Println(nowtime)
+	// 	fmt.Println(nowtime.Format(layout1))
+	// 	fmt.Println(nowtime.Format(layout2))
+	// 	fmt.Println(nowtime.Format(layout3))
+	// 	fmt.Println(nowtime.Format(time.RFC3339))
 	mylogger("aaaaa my log bbbbb")
 	mylogger("aaaaa my log cccccc")
 }
 
-func mylogger (msg string){
+func mylogger(msg string) {
 	var logent string
 	nowtime := time.Now()
-    logent  = nowtime.Format(time.RFC3339) + " " + msg
-	fmt.Println (logent)
+	logent = nowtime.Format(time.RFC3339) + " " + msg
+	fmt.Println(logent)
 }
